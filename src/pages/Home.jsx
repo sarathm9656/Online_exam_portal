8b40f203fbef2e2f8ab445da91f835c6b9da5549
Sarath M <sarathmullath9656@gmail.com>
2025-01-27 23:27:09 +0530

    final3 commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
 
       <div className="aq">
         <div className="home_left_part">
-
+       
     <div className="home_para_section">
             <h1 className="home_page_image_head">Online
                Education</h1>
